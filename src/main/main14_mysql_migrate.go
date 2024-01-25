86978191c9be03083c2ef4ec14edd00a4f53344a
bofei <bofei222@163.com>
2024-01-25 15:31:32 +0800

    添加 migrate

--- src/main/main14_mysql_migrate.go
+++ src/main/main14_mysql_migrate.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"database/sql"
-	"github.com/golang-migrate/migrate/v4"
-
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/mysql"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
@@ -22,7 +21,24 @@ func main() {
 	}
 	err = m.Up()
 	if err != nil {
-		panic(err)
+		if err.Error() != "no change" {
+			panic(err)
+		}
+	}
+
+	db2, _ := sql.Open("mysql", "root:sany_root@tcp(10.162.4.9:3306)/sany_scada?multiStatements=true")
+	driver2, _ := mysql.WithInstance(db2, &mysql.Config{})
+	m2, err2 := migrate.NewWithDatabaseInstance(
+		"file://migrations/sany_scada/",
+		"mysql",
+		driver2,
+	)
+	if err2 != nil {
+		panic(err2)
+	}
+	err2 = m2.Up()
+	if err2 != nil {
+		panic(err2)
 	}
 
 }
